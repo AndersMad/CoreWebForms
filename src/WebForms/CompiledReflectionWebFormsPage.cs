dd105910c035877e6e4779f5d336e9827942ad2d
Anders Madsen <thisisafish@gmail.com>
2026-04-03 15:09:48 +0200

    fix CoreWebForms release build by correcting usings

--- src/WebForms/CompiledReflectionWebFormsPage.cs
+++ src/WebForms/CompiledReflectionWebFormsPage.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SystemWebAdapters.HttpHandlers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Primitives;
 using WebForms.Features;
